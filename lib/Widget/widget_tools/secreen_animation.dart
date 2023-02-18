bb97ab00003f6c636fb3c11c92d772310d80aa2d
yusefHajeb <watanalihsas@gmail.com>
2023-02-18 18:01:53 +0300

    button in home page use for  move to page select

---
+++ lib/Widget/widget_tools/secreen_animation.dart
@@ -0,0 +1,171 @@
+import 'dart:async';
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyCustomWidget extends StatefulWidget {
+  @override
+  _MyCustomWidgetState createState() => _MyCustomWidgetState();
+}
+
+class _MyCustomWidgetState extends State<MyCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            width: 400,
+            margin: EdgeInsets.only(top: 0, right: 30, left: 30, bottom: 30),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 6),
+                  blurRadius: 17,
+                  spreadRadius: -20,
+                  color: Colors.black,
+                ),
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(29.5),
+            ),
+            //  color: Colors.white, borderRadius: BorderRadius.circular(29.5)),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: "ابحث برقم الجواز",
+                icon: Icon(Icons.search),
+                border: InputBorder.none,
+                suffixIcon: OpenContainer(
+                  closedBuilder: (_, openContainer) {
+                    return Container(
+                      height: 80,
+                      width: 80,
+                      child: Center(
+                        child: Text(
+                          'بحث',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  openColor: Colors.white,
+                  closedElevation: 20,
+                  closedShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  transitionDuration: Duration(milliseconds: 700),
+                  openBuilder: (_, closeContainer) {
+                    return SecondScreen();
+                  },
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  bool _a = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 700), () {
+      setState(() {
+        _a = !_a;
+      });
+    });
+    Timer(Duration(milliseconds: 2000), () {
+      Navigator.of(context)
+          .pushReplacement(SlideTransitionAnimation(SecondPage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 2000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            width: _a ? _width : 0,
+            height: _height,
+            color: Colors.black,
+          ),
+          Center(
+            child: Text(
+              'جاري البحث',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SlideTransitionAnimation extends PageRouteBuilder {
+  final Widget page;
+
+  SlideTransitionAnimation(this.page)
+      : super(
+            pageBuilder: (context, animation, anotherAnimation) => page,
+            transitionDuration: Duration(milliseconds: 2000),
+            transitionsBuilder: (context, animation, anotherAnimation, child) {
+              animation = CurvedAnimation(
+                curve: Curves.fastLinearToSlowEaseIn,
+                parent: animation,
+              );
+              return SlideTransition(
+                position: Tween(begin: Offset(1.0, 0.0), end: Offset(0.0, 0.0))
+                    .animate(animation),
+                textDirection: TextDirection.rtl,
+                child: page,
+              );
+            });
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('الاستعلام'),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          systemOverlayStyle: SystemUiOverlayStyle.light),
+      body: Center(
+        child: Text(
+          "تم ",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
