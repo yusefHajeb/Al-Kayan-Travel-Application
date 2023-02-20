2b37d638c8c462c91f366cb8661895655ac17b04
yusefHajeb <watanalihsas@gmail.com>
2023-02-21 02:53:13 +0300

    shadaw to wiget in servece page

--- lib/screen/sirvece_screen.dart
+++ lib/screen/sirvece_screen.dart
@@ -3,6 +3,7 @@ import 'package:page_transition/page_transition.dart';
 // import 'package:yah_app/screen/first_screen.dart';
 import 'package:yah_app/screen/sirvec_page.dart';
 import 'dealetes.dart';
+import 'first_screen.dart';
 
 class SrvessScreen extends StatelessWidget {
   static const routeName = 'servece_screen';
@@ -25,31 +26,60 @@ class SrvessScreen extends StatelessWidget {
     );
   }
 
-  // Widget buildContent(Widget child) {
-  //   return Container(
-  //       padding: EdgeInsets.all(20),
-  //       decoration: BoxDecoration(
-  //         color: Colors.white10,
-  //         borderRadius: BorderRadius.circular(12),
-  //         border:
-  //             BoxBorder.lerp(Border.all(width: 1.0), Border.all(width: 1.0), 3),
-  //       ),
-  //       margin: EdgeInsets.all(10),
-  //       height: 200,
-  //       width: 300,
-  //       child: child);
-  // }
-
   @override
   Widget build(BuildContext context) {
     // final mealId = ModalRoute.of(context)?.settings.arguments;
     // final selectedMeal = DUMMY_MEALS.firstWhere((meal) => mealId == meal.id);
-
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
+            Stack(children: <Widget>[
+              Container(
+                height: size.height / 4,
+                width: double.infinity,
+                child: Image.asset(
+                  "assest/image/airplane1.jpg",
+                  fit: BoxFit.fill,
+                ),
+                // margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    // color: Color.fromRGBO(255, 189, 0, 1),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50))),
+              ),
+              Positioned(
+                top: 50,
+                right: 20,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, '/');
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                height: 20,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25.0),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
             SizedBox(
               height: MediaQuery.of(context).size.height,
               child: ListView.builder(
@@ -58,15 +88,6 @@ class SrvessScreen extends StatelessWidget {
                   return InkWell(
                     onTap: () {
                       showService(ctx, index);
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => Container(child: PageService()),
-                      //     settings: RouteSettings(
-                      //       arguments: index,
-                      //     ),
-                      //   ),
-                      // );
                     },
                     child: Card(
                       shape: RoundedRectangleBorder(
