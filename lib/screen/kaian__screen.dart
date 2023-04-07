aaf2bbfa172b9891b32345ebde4aa42490dc4df3
yusefHajeb <watanalihsas@gmail.com>
2023-04-08 02:28:24 +0300

    change to stateless

--- lib/screen/kaian__screen.dart
+++ lib/screen/kaian__screen.dart
@@ -16,30 +16,27 @@ import 'package:yah_app/styles/style.dart';
 //   Product(this.image, this.name, this.discrib);
 // }
 
-class Screen2 extends StatefulWidget {
-  @override
-  State<Screen2> createState() => _Screen2();
+class Screen2 extends StatelessWidget {
   static const routeName = 'screen2 ';
-}
 
-class _Screen2 extends State<Screen2> with TickerProviderStateMixin {
   late final AnimationController _controller;
 
   late Animation<double> _fadeInFadeOut;
-  // @override
-  // GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+// @override
+// GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    final numTab = ModalRoute.of(context)?.settings.arguments.toString();
+    final numTab = Provider.of<ProviderService>(context).getNumberScreen();
+
     //set val to index in provider
     Provider.of<ProviderService>(context, listen: false)
         .setIndex(numTab.toString());
-    // final String index = Provider.of<ProviderService>(context).getIndex();
 
     List<dynamic> check = Provider.of<ProviderService>(context).check();
 
     return Scaffold(
+      // appBar: ,
       backgroundColor: Colors.white,
       body: ListView(
         shrinkWrap: true,
@@ -93,91 +90,30 @@ class _Screen2 extends State<Screen2> with TickerProviderStateMixin {
               ),
             ),
           ]),
-          // myAppBarServece(size: size),
-          // Transform.translate(
-          //   offset: Offset(0, -35),
-          //   child: Container(
-          //     margin: EdgeInsets.symmetric(horizontal: 20),
-          //     decoration: BoxDecoration(
-          //       color: Colors.white,
-          //       borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          //       boxShadow: [
-          //         BoxShadow(
-          //           offset: Offset(0, 10.0),
-          //           blurRadius: 20.0,
-          //           color: Color.fromARGB(255, 158, 158, 158),
-          //         ),
-          //       ],
-          //     ),
-          //     height: 60,
-          //     child: Container(
-          //       margin: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-          //       padding: EdgeInsets.all(5),
-          //       height: 60,
-          //       child: Row(
-          //         //listView
-          //         // scrollDirection: Axis.horizontal,
-          //         children: ['مايميزنا', 'شركائنا', 'فروعنا']
-          //             .map((e) => Container(
-          //                 margin: EdgeInsets.symmetric(
-          //                     vertical: 8.0, horizontal: 8.0),
-          //                 child: Bouncing(
-          //                   child: OutlinedButton(
-          //                     onPressed: () {},
-          //                     style: OutlinedButton.styleFrom(
-          //                         shape: RoundedRectangleBorder(
-          //                           borderRadius: BorderRadius.circular(18.0),
-          //                         ),
-          //                         side: BorderSide(
-          //                             width: 1, color: Colors.black)),
-          //                     child: Text(
-          //                       e,
-          //                       style:
-          //                           TextStyle(color: bBackDark, fontSize: 15),
-          //                     ),
-          //                     // onHover: (context){},
-          //                   ),
-          //                   onPress: () {
-          //                     setState(() {
-          //                       if (e == 'مايميزنا')
-          //                         index = '1';
-          //                       else if (e == 'شركائنا')
-          //                         index = '2';
-          //                       else if (e == "فروعنا") index = '3';
-          //                       // check();
-          //                       index = "";
-          //                     });
-          //                   },
-          //                 )))
-          //             .toList(),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          SizedBox(
+          const SizedBox(
             height: 4,
           ),
           Column(
               children: check
                   .map((e) => AnimationConfiguration.staggeredList(
                       position: 2,
-                      delay: Duration(milliseconds: 100),
+                      delay: const Duration(milliseconds: 100),
                       child: SlideAnimation(
-                        duration: Duration(milliseconds: 2500),
+                        duration: const Duration(milliseconds: 2500),
                         curve: Curves.fastLinearToSlowEaseIn,
                         child: SlideAnimation(
-                          duration: Duration(milliseconds: 2500),
+                          duration: const Duration(milliseconds: 2500),
                           curve: Curves.fastLinearToSlowEaseIn,
                           child: FadeInAnimation(
                             curve: Curves.fastLinearToSlowEaseIn,
-                            duration: Duration(milliseconds: 2500),
+                            duration: const Duration(milliseconds: 2500),
                             child: Container(
-                              margin: EdgeInsets.symmetric(
+                              margin: const EdgeInsets.symmetric(
                                   vertical: 8, horizontal: 20),
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(13),
-                                boxShadow: [
+                                boxShadow: const [
                                   BoxShadow(
                                     offset: Offset(0, 6),
                                     blurRadius: 10,
