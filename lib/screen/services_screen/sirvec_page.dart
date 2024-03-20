ac74b1de16c2a8d9745231fe7ff9b8610a737d8c
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-03-21 01:07:14 +0300

    fix style and reformat

--- lib/screen/services_screen/sirvec_page.dart
+++ lib/screen/services_screen/sirvec_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -47,183 +49,188 @@ class _PageService extends State<PageService> {
     //عند الا
     final int sectionIndex = Provider.of<ServicesProvider>(context).getIndex();
     return WillPopScope(
-        onWillPop: () async {
-          return await Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (_) => const ShowService()));
-        },
-        child: Scaffold(
-          // resizeToAvoidBottomInset: true,
-          backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            child: SizedBox(
-              height: size.height,
-              child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  // scrollDirection: Axis.vertical,
-                  // shrinkWrap: true,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Stack(children: <Widget>[
-                        Container(
-                          height: size.height / 3,
-                          width: double.infinity,
-                          child: Container(
-                            width: double.infinity,
-                            // height: size.height / 4,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                              image: CachedNetworkImageProvider(
-                                  myList[sectionIndex]['imgUrl']),
-                              fit: BoxFit.cover,
-                              // dataServices[index].imgUrl,
-                            )),
-                          ),
-                          decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(50),
-                                  bottomRight: Radius.circular(50))),
+      onWillPop: () async {
+        return await Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => const ShowService()));
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Column(children: [
+              Expanded(
+                flex: 1,
+                child: Stack(children: <Widget>[
+                  Container(
+                    height: size.height / 3,
+                    width: double.infinity,
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(
+                              myList[sectionIndex]['imgUrl']),
+                          fit: BoxFit.cover,
                         ),
-                        Positioned(
-                          top: 50,
-                          right: 20,
-                          child: IconButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) => const ShowService()));
-                              // Navigator.popAndPushNamed(context, ShowService.routeName);
-                            },
-                            icon: const Icon(
-                              Icons.arrow_back_ios,
-                              color: Colors.black,
-                            ),
+                      ),
+                    ),
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50))),
+                  ),
+                  Positioned(
+                    top: 50,
+                    right: 20,
+                    child: IconButton(
+                      alignment: Alignment.center,
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const ShowService()));
+                      },
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            const CircleBorder(),
                           ),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(0)),
+                          elevation: MaterialStateProperty.all(0),
+                          minimumSize:
+                              MaterialStateProperty.all(const Size(34, 34)),
+                          visualDensity: VisualDensity.adaptivePlatformDensity,
+                          alignment: Alignment.center,
+                          shadowColor: MaterialStateProperty.all(Colors.black),
+                          backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 218, 172, 31),
+                          )),
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: -2,
+                    height: 25,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25.0),
+                          topRight: Radius.circular(25.0),
                         ),
-                        Positioned(
-                          left: 0,
-                          right: 0,
-                          bottom: -2,
-                          height: 25,
-                          child: Container(
-                            // height: 600,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(25.0),
-                                topRight: Radius.circular(25.0),
-                              ),
-                            ),
-                          ),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Transform.translate(
+                offset: const Offset(0, -50),
+                child: Container(
+                  width: size.width / 1.50,
+                  padding: const EdgeInsets.all(13),
+                  // color: Colors.white,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          offset: Offset(0, 6),
+                          blurRadius: 17,
+                          spreadRadius: -6,
+                          color: Colors.black,
                         )
                       ]),
+                  child: Center(
+                    child: Text(
+                      myList[sectionIndex]['title'],
+                      // data[sectionIndex].title,
+                      textAlign: TextAlign.center,
+                      style: header2,
                     ),
-                    Transform.translate(
-                      offset: const Offset(0, -50),
-                      child: Container(
-                        width: size.width / 1.50,
-                        padding: const EdgeInsets.all(13),
-                        // color: Colors.white,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(13),
-                            color: Colors.white,
-                            boxShadow: const [
-                              BoxShadow(
-                                offset: Offset(0, 6),
-                                blurRadius: 17,
-                                spreadRadius: -6,
-                                color: Colors.black,
-                              )
-                            ]),
-                        child: Center(
-                          child: Text(
-                            myList[sectionIndex]['title'],
-                            // data[sectionIndex].title,
-                            textAlign: TextAlign.center,
-                            style: header2,
-                          ),
+                  ),
+                ),
+              ),
+              //-=============================================
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                  // height: size.height / 2.6,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    child: Text(
+                      myList[sectionIndex]['paragraph'],
+                      style: paragraph.copyWith(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 3, horizontal: 20),
+                        child: Text(
+                          "مزيد من الخدمات",
+                          style: header2,
                         ),
                       ),
-                    ),
-                    //-=============================================
-                    Expanded(
-                      flex: 2,
-                      child: SizedBox(
-                        height: size.height / 2.6,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 15),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 10),
-                            child: Text(
-                              myList[sectionIndex]['paragraph'],
-                              // data[sectionIndex].paragtaph,
-                              style: paragraph,
-                            ),
-                          ),
-                        ),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    Expanded(
-                        flex: 1,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 3, horizontal: 20),
-                              child: Text(
-                                "مزيد من الخدمات",
-                                style: header2,
+                      SizedBox(
+                          height: 80.0,
+                          child: AnimationConfiguration.synchronized(
+                            duration: const Duration(milliseconds: 600),
+                            child: SlideAnimation(
+                              curve: Curves.easeInCirc,
+                              child: FadeInAnimation(
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  shrinkWrap: true,
+                                  physics: const BouncingScrollPhysics(),
+                                  itemBuilder: (context, index) {
+                                    return AnimationConfiguration.staggeredList(
+                                      position: index,
+                                      duration:
+                                          const Duration(milliseconds: 600),
+                                      child: SlideAnimation(
+                                        horizontalOffset: 30.0,
+                                        child: FadeInAnimation(
+                                          delay:
+                                              const Duration(milliseconds: 200),
+                                          child: Bouncing(
+                                              onPress: () {},
+                                              child: (CardBottom(
+                                                  sectionIndex: index))),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  itemCount: data.length,
+                                ),
                               ),
                             ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            SizedBox(
-                                height: 80.0,
-                                child: AnimationConfiguration.synchronized(
-                                  duration: const Duration(milliseconds: 600),
-                                  child: SlideAnimation(
-                                    curve: Curves.easeInCirc,
-                                    child: FadeInAnimation(
-                                      child: ListView.builder(
-                                        scrollDirection: Axis.horizontal,
-                                        shrinkWrap: true,
-                                        physics: const BouncingScrollPhysics(),
-                                        itemBuilder: (context, index) {
-                                          return AnimationConfiguration
-                                              .staggeredList(
-                                            position: index,
-                                            duration: const Duration(
-                                                milliseconds: 600),
-                                            child: SlideAnimation(
-                                              horizontalOffset: 30.0,
-                                              child: FadeInAnimation(
-                                                delay: const Duration(
-                                                    milliseconds: 200),
-                                                child: Bouncing(
-                                                    onPress: () {},
-                                                    child: (CardBottom(
-                                                        sectionIndex: index))),
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                        itemCount: data.length,
-                                      ),
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        ))
-                  ]),
-            ),
+                          )),
+                    ],
+                  ))
+            ]),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
@@ -246,7 +253,7 @@ class CardBottom extends StatelessWidget {
         aspectRatio: 3 / 1,
         child: Container(
           margin: const EdgeInsets.only(left: 5, right: 10, bottom: 10),
-          padding: const EdgeInsets.all(9),
+          // padding: const EdgeInsets.all(9),
           decoration: BoxDecoration(
               border: Border.all(color: Colors.grey),
               borderRadius: BorderRadius.circular(15),
@@ -259,36 +266,44 @@ class CardBottom extends StatelessWidget {
                   color: Colors.black,
                 )
               ]),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Container(
-              width: 60,
-              height: 40,
-              // padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: const Color.fromARGB(255, 40, 39, 37)),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                    image: CachedNetworkImageProvider(
-                        myList[sectionIndex]['imgUrl']),
-                    fit: BoxFit.fill,
-                    // dataServices[index].imgUrl,
-                  )),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                // width: 70,
+                // height: 50,
+                // padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromARGB(255, 40, 39, 37)),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Container(
+                    // width: 100,
+                    // height: 100,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: CachedNetworkImageProvider(
+                          myList[sectionIndex]['imgUrl']),
+                      fit: BoxFit.fill,
+                      // dataServices[index].imgUrl,
+                    )),
+                  ),
                 ),
               ),
             ),
-            Text(
-              myList[sectionIndex]['title'],
-              // data[sectionIndex].title,
-              style: paragraph,
-              //  TextStyle(
-              //     color: Color.fromARGB(255, 54, 52, 52), fontSize: 13),
+            SizedBox(width: 10),
+            Expanded(
+              flex: 4,
+              child: Text(
+                myList[sectionIndex]['title'],
+                // data[sectionIndex].title,
+                style: paragraph2,
+                //  TextStyle(
+                //     color: Color.fromARGB(255, 54, 52, 52), fontSize: 13),
+              ),
             ),
           ]),
         ),
